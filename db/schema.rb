28d8cbf1d22d615d9728a759da9432d563dd0651
MariDani <danielova.mariana@gmail.com>
2014-09-15 13:27:56 +0200

    user info

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915110329) do
+ActiveRecord::Schema.define(version: 20140915112042) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20140915110329) do
     t.datetime "updated_at"
     t.boolean  "aproved"
     t.boolean  "admin"
+    t.string   "name"
+    t.string   "institution"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
